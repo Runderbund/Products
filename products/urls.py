2a5e4cf0be7df321361fc4ae2fad45cea575e806
unknown <sean.herbison@gmail.com>
2023-03-27 10:43:31 -0700

    Registered admin site in admin.py and added model and serializer for Product.

---
+++ products/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path ('', views.cars_list),
+    path ('<int:pk>/', views.car_detail),
+    path ('<str:make>/', views.cars_by_make)
+]
--- products_project/settings.py
+++ products_project/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4iv^ge+g!ejj3+!g!&yy8yba1ubr4+=3g16+6n3%4l*w-i9m&z'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -69,16 +66,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'products_project.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
