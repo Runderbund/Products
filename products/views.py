ca496109b39c397212052760c44d0e15029f3894
unknown <sean.herbison@gmail.com>
2023-03-28 12:45:21 -0700

    Adds all the views required by the project requirements and migrates to the database.

--- products/views.py
+++ products/views.py
@@ -7,7 +7,6 @@ from .models import Product
 
 @api_view(['GET', 'POST'])
 def products_list(request):
-
     if request.method == 'GET':
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
@@ -22,14 +21,17 @@ def products_list(request):
 @api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
+
     if request.method == 'GET':
         serializer = ProductSerializer(product)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
     elif request.method == 'PUT':
         serializer = ProductSerializer(product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
     elif request.method == 'DELETE':
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
